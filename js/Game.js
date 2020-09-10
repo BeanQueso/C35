a47331018488d310dc3dc02e84f9fa5e29e79f22
eshaant7 <eshaant7@gmail.com>
2020-09-10 10:38:20 +0200

    35

---
+++ js/Game.js
@@ -0,0 +1,24 @@
+class Game{
+    constructor(){
+
+    }
+    getState(){
+        var stateRef = database.ref('gameState');
+        stateRef.on("value", function(data){
+            gameState = data.val();
+        });
+    }
+    update(state){
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    start(){
+    if(gameState === 0){
+        form = new Form();
+        form.display();
+        player = new Player();
+        player.getCount();
+}
+    }
+}
